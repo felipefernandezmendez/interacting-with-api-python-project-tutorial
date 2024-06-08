e1fc706b45ea1da511902e8e814c8373bd766221
felipefernandezmendez <150546779+felipefernandezmendez@users.noreply.github.com>
2024-06-08 18:15:11 +0000

    Entrega API

--- src/app.py
+++ src/app.py
@@ -5,3 +5,11 @@ from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
